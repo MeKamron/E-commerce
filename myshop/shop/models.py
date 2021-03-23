e0564a5fdf59c8c9609961db4dad16a284f4fa64
mekamron <kamronbek1709@gmail.com>
2021-03-23 15:24:28 +0500

    Create templates

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
@@ -11,6 +12,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("shop:product_list_by_category", args=[self.slug])
+
     
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
@@ -21,6 +26,16 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=20, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
 
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("shop:product_detail", args=[self.id, self.slug])
+
+        
